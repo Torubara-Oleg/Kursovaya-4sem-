eece6baa7ab54eda14be7a9eb7144e57eec422de
Oleg Torubara <79332163+Torubara-Oleg@users.noreply.github.com>
2021-07-22 19:37:20 +0300

    Add files via upload

---
+++ GetFile/GetFile/GetFile.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <fstream>
+
+int main()
+{
+    std::ofstream file; 
+    file.open("d:\\Desktop\\Poluchatel.txt", std::ios::out);
+
+    OpenClipboard(NULL);
+    std::string stroka;
+
+    HANDLE hData = GetClipboardData(CF_TEXT);
+    char* ch1Buffer = (char*)GlobalLock(hData);
+    stroka = ch1Buffer;
+    GlobalUnlock(hData);
+    CloseClipboard();
+
+    std::cout << stroka;
+    
+    file << stroka; 
+    file.close();
+}
