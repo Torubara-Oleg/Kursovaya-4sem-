d1bde2b2b8461cd01ae3d6c2c3ea9b1edd3dbafa
Oleg Torubara <79332163+Torubara-Oleg@users.noreply.github.com>
2021-07-22 19:36:41 +0300

    Add files via upload

---
+++ SaveFile/SaveFile/SaveFile.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <fstream>
+
+#pragma warning(disable : 4996)
+
+
+
+int main()
+{
+    std::fstream file;
+
+    std::string str;
+
+    std::string fullStroka = "";
+
+    HGLOBAL hgBuffer;
+    char* chBuffer;
+
+    std::string separator = "////";
+    std::string path = "c:\\SaveFile\\BMP_КОТ.bmp";
+
+    file.open("c:\\SaveFile\\file.txt",std::ios::in);
+
+    while (!file.eof())
+    {
+        file >> str;
+        fullStroka += str;
+        fullStroka += " ";
+    }
+
+    
+    fullStroka += separator;
+    fullStroka += " ";
+    fullStroka += path;
+    
+
+    OpenClipboard(NULL);
+
+    EmptyClipboard();
+    hgBuffer = GlobalAlloc(GMEM_DDESHARE, fullStroka.length() + 1);
+    chBuffer = (char*)GlobalLock(hgBuffer);
+    strcpy(chBuffer, fullStroka.c_str());
+    GlobalUnlock(hgBuffer);
+    SetClipboardData(CF_TEXT, hgBuffer);
+    CloseClipboard();
+
+    
+    file.close();
+}
